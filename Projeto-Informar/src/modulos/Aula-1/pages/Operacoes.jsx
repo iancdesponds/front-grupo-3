820d4194286b55733971b66d05c6167579693491
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-15 17:50:22 -0300

    Update Operacoes.jsx

--- Projeto-Informar/src/modulos/Aula-1/pages/Operacoes.jsx
+++ Projeto-Informar/src/modulos/Aula-1/pages/Operacoes.jsx
@@ -13,72 +13,18 @@ function Operacoes() {
       <QuestionCard
         questionTitle="Exercício 1"
         questionDescription="Considere o seguinte código:"
-        codeSnippet={`x = 12
-if x < 10:
-    print('O numero é menor que 10.')
-else:
-    print('O número é maior ou igual a 10.')`}
+        codeSnippet={`x = 2 + 2 * 4
+print(x)`}
         questionText="O que esse código imprime no terminal?"
         options={[
-          "O número é maior ou igual a 10.",
-          "O número é menor que 10.",
-          "12",
-          "Não será impresso nada no terminal."
+          "8",
+          "16",
+          "10",
+          "12"
         ]}
-        correctIndex={0}
-        explanation="O código checa se x é menor que 10 quando passa pelo if, como x é maior que 10, ele acaba imprimindo a outra linha."
+        correctIndex={2}
+        explanation="O programa faz primeiro a mulitplicação e depois a adição, resultando em 10."
         localStorageKey="exercicio1_operacoes"
-      />
-      <QuestionCard
-        questionTitle="Exercício 2"
-        questionDescription="Considere o seguinte código:"
-        codeSnippet={`a = 5
-b = 3
-c = a + b
-if c > 10:
-    print(b)
-else:
-    print(c)`}
-        questionText="O que esse código faz?"
-        options={[
-          "",
-          "",
-          "",
-          ""
-        ]}
-        correctIndex={0}
-        explanation=""
-        localStorageKey="exercicio2_operacoes"
-      />
-      <QuestionCard
-        questionTitle="Exercício 2"
-        questionDescription="Considere o seguinte código:"
-        codeSnippet={``}
-        questionText="O que esse código faz?"
-        options={[
-          "",
-          "",
-          "",
-          ""
-        ]}
-        correctIndex={0}
-        explanation=""
-        localStorageKey="exercicio2_operacoes"
-      />
-      <QuestionCard
-        questionTitle="Exercício 2"
-        questionDescription="Considere o seguinte código:"
-        codeSnippet={``}
-        questionText="O que esse código faz?"
-        options={[
-          "",
-          "",
-          "",
-          ""
-        ]}
-        correctIndex={0}
-        explanation=""
-        localStorageKey="exercicio2_operacoes"
       />
       <QuestionCard
         questionTitle="Exercício 2"
