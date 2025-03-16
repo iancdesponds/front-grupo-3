ba2255164008f7349b93f3b05f04f85f2c24c01c
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-15 22:42:07 -0300

    Create Strings.jsx

---
+++ Projeto-Informar/src/modulos/Aula-3/pages/Strings.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import QuestionCard from '../../../components/QuestionCard';
+
+function Strings() {
+  return (
+    <div>
+      <h1>Aula 3 - Strings</h1>
+      <p>
+      Vamos relembrar do que aprendemos na aula a respeito de strings em Python.
+      </p>
+      <QuestionCard
+        questionTitle="Exercício 1"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={``}
+        questionText="Qual será a mensagem impressa no terminal?"
+        options={[
+          "",
+          "",
+          "",
+          ""
+        ]}
+        correctIndex={1}
+        explanation=""
+        localStorageKey="exercicio1_strings"
+      />
+      <QuestionCard
+        questionTitle="Exercício 2"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={``}
+        questionText=""
+        options={[
+          "",
+          "",
+          "",
+          ""
+        ]}
+        correctIndex={2}
+        explanation=""
+        localStorageKey="exercicio2_strings"
+      />
+      <QuestionCard
+        questionTitle="Exercício 3"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={``}
+        questionText="Qual será a mensagem impressa no terminal?"
+        options={[
+          "",
+          "",
+          "",
+          ""
+        ]}
+        correctIndex={2}
+        explanation=""
+        localStorageKey="exercicio3_strings"
+      />
+      <QuestionCard
+        questionTitle="Exercício 4"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={``}
+        questionText="Qual será a mensagem impressa no terminal?"
+        options={[
+          "",
+          "",
+          "",
+          ""
+        ]}
+        correctIndex={2}
+        explanation=""
+        localStorageKey="exercicio4_strings"
+      />
+      <QuestionCard
+        questionTitle="Exercício 5"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={``}
+        questionText="Qual será a mensagem impressa no terminal?"
+        options={[
+          "",
+          "",
+          "",
+          ""
+        ]}
+        correctIndex={2}
+        explanation=""
+        localStorageKey="exercicio5_strings"
+      />
+    </div>
+  );
+}
+
+export default Listas;
