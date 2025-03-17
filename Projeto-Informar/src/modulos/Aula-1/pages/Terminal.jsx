86a1002d9a87c4d6b003b9b9d5c34868a2620fb1
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-17 12:59:41 -0300

    Update Terminal.jsx

--- Projeto-Informar/src/modulos/Aula-1/pages/Terminal.jsx
+++ Projeto-Informar/src/modulos/Aula-1/pages/Terminal.jsx
@@ -1,16 +1,46 @@
-import React from "react";
-import NextPage from "../../../components/Next_page";
+import React from 'react';
+import QuestionCard from '../../../components/QuestionCard';
 
 function Terminal() {
   return (
     <div>
       <h1>Aula 1 - Terminal</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        ullamcorper enim at nulla faucibus luctus. Quisque pulvinar augue non
-        justo finibus cursus.
+      Vamos relembrar do que aprendemos na aula a respeito do terminal em Python.
       </p>
-      <NextPage nomeDaPagina={'Aula - 2'} link={'/aula-2/inicio'}/>
+      <QuestionCard
+        questionTitle="Exercício 1"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={`nome = input('Qual é o seu nome? ')
+print(nome)`}
+        questionText="O que esse código faz?"
+        options={[
+          "Imprime 'nome'.",
+          "Não faz nada.",
+          "Pede um nome para o usuário e depois o imprime.",
+          "O código da algum erro."
+        ]}
+        correctIndex={2}
+        explanation="input('texto') pede uma informação ao usuário pelo terminal." 
+        localStorageKey="exercicio1_terminal"
+      />
+      <QuestionCard
+        questionTitle="Exercício 2"
+        questionDescription="Considere o seguinte código:"
+        codeSnippet={`a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+print(2*a-b)`}
+        questionText="Se o usuário digitou 5 no terminal, que outro número ele terá que digitar para o código imprimir 8?"
+        options={[
+          "3",
+          "2",
+          "5",
+          "1"
+        ]}
+        correctIndex={1}
+        explanation="O variável b vai se tornar o que o usuário digitar."
+        localStorageKey="exercicio2_terminal"
+      />
     </div>
   );
 }
