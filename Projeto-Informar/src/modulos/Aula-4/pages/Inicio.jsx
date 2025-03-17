a2419d8ddea3c0cf624f975b9ef3c60a91ea14de
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-17 13:18:16 -0300

    Update Inicio.jsx

--- Projeto-Informar/src/modulos/Aula-4/pages/Inicio.jsx
+++ Projeto-Informar/src/modulos/Aula-4/pages/Inicio.jsx
@@ -1,13 +1,12 @@
-import React from "react";
+import React from 'react';
 
 function InicioAula4() {
   return (
     <div>
       <h1>Aula 4 - Início</h1>
+      <h2>While</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        ullamcorper enim at nulla faucibus luctus. Quisque pulvinar augue non
-        justo finibus cursus.
+      <strong>O while funciona de maneira similar ao if.</strong> Entretanto, ao chegar no final de um bloco de código desse tipo, o programa <strong>reavalia se a condição inicialmente proposta ainda está satisfeita.</strong> O while tem diversos usos e também pode ser usado como um contador.
       </p>
     </div>
   );
