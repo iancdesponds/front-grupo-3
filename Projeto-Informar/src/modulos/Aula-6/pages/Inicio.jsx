13f49c26fc2745ebf54ec52b2f2069970254306c
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-17 13:23:14 -0300

    Update Inicio.jsx

--- Projeto-Informar/src/modulos/Aula-6/pages/Inicio.jsx
+++ Projeto-Informar/src/modulos/Aula-6/pages/Inicio.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 
 function InicioAula6() {
   return (
     <div>
       <h1>Aula 6 - Início</h1>
+      <h2>Funções</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        ullamcorper enim at nulla faucibus luctus. Quisque pulvinar augue non
-        justo finibus cursus.
+      Uma função em Python é um bloco de <strong>código definido (ou seja, que tem um nome) que realiza alguma tarefa e pode ser executado várias vezes ao longo de todo o código</strong> se ele for chamado.
+As funções servem para facilitar a programação, ajudando a evitar a repetição de blocos de código, a existência de códigos grandes e confusos.
+As funções precisam ser definidas e atuam sobre qualquer tipo de variável, desde que os seus comandos respeitem os limites dessa variável. <strong>Todas as funções devem retornar algo</strong>, que por sua vez também pode ser qualquer tipo de variável.
+Ou seja, elas <strong>funcionam como um cozinheiro executando uma receita de bolo</strong>. Você fornece para ele os ingredientes e no final ele te retorna o bolo pronto.
       </p>
     </div>
   );
