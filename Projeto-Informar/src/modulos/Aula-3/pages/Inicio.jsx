fbd17ed6ab57543e854738ddf9f34b1ce6507158
LeonardoMA07 <leonardomorettialva@gmail.com>
2025-03-17 13:14:47 -0300

    Update Inicio.jsx

--- Projeto-Informar/src/modulos/Aula-3/pages/Inicio.jsx
+++ Projeto-Informar/src/modulos/Aula-3/pages/Inicio.jsx
@@ -4,10 +4,17 @@ function InicioAula3() {
   return (
     <div>
       <h1>Aula 3 - Início</h1>
+      <h2>Listas</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-        ullamcorper enim at nulla faucibus luctus. Quisque pulvinar augue non
-        justo finibus cursus.
+      Como já foi definido na aula de variáveis, as listas em Python são um tipo de variável que representa uma lista de elementos variados, ou seja, uma lista que pode conter todos os elementos descritos acima..
+      </p>
+      <p>
+        Assim como as strings, cada elemento de uma lista possui um índice e é por meio dele que podemos acessar aquele elemento, nos permitindo até fazer alterações sobre o mesmo.
+      </p>
+      <h2>Strings</h2>
+      <p>
+      String é um tipo de variável em python que representa um encadeamento de caracteres, ou seja, representa valores em formato de <strong>texto, palavras, frase.</strong> Uma string pode representar um número, mas ao ele estar nesse formato não será possível efetuar nenhuma conta com ele. <strong>Cada elemento de uma string possui um índice (index)</strong> que se refere a ela dentro daquela variável.
+Esse índice começa em zero  e vai até o elemento final daquela string.
       </p>
     </div>
   );
