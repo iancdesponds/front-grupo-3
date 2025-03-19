557cc392e30f34a26ac148dc85b7e9e08004580e
Yuri Henrique <yurihcs@al.insper.edu.br>
2025-03-19 13:20:32 -0300

    Fix - Inverte_string

--- Projeto-Informar/src/modulos/Exercícios 1/pages/Ex_inverte_string.jsx
+++ Projeto-Informar/src/modulos/Exercícios 1/pages/Ex_inverte_string.jsx
@@ -6,7 +6,7 @@ function Ex_inverte_string() {
     <div>
       <h1>Exercício 1 - Função Inverte String</h1>
       <p>
-        Implemente a função <code>reserve_string()</code> que recebe uma string
+        Implemente a função <code>inverte_string()</code> que recebe uma string
         e retorna essa string invertida, ou seja, de trás para frente.
       </p>
       <CodeEditor exerciseId={5} />
